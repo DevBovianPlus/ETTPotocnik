1c7bed050f5431ae6c912ee8aee6f5176f74dc91
Martin Polegek <martin@bovianplus.si>
2021-03-12 11:44:15 +0100

    dodaj

---
+++ ETT_SchedularLibrary/Users.cs
@@ -0,0 +1,24 @@
+﻿using DevExpress.Xpo;
+using ETT_DAL.Abstract;
+using ETT_DAL.Concrete;
+using ETT_DAL.ETTPotocnik;
+using ETT_DAL.Helpers;
+using System;
+using System.Collections.Generic;
+
+namespace ETT_SchedularLibrary
+{
+    public class Users
+    {
+        private IEmployeeRepository utilityRepo;
+        private Session session;
+
+        public Employee GetEmployee()
+        {
+            session = XpoHelper.GetNewSession();
+            utilityRepo = new EmployeeRepository(session);
+
+            return utilityRepo.GetEmployeeByID(2, session);
+        }
+    }
+}
